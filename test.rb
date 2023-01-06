caad0f22834fd96f848d6919b26548f5944a7113
Aminu Rabiu <arabiu033@gmail.com>
2023-01-06 10:25:29 +0100

    Add the tests for the fizzbuzz method

--- test.rb
+++ test.rb
@@ -1,22 +1,21 @@
-require './solver.rb'
+require './solver'
 solver = Solver.new
 
 describe 'factorial' do
-    it 'Factorial of a number' do
-        expect(solver.factorial(5)).to eq(120)
-    end
-    it 'Factorial of zero' do
-        expect(solver.factorial(0)).to eq(1)
-    end
+  it 'Factorial of a number' do
+    expect(solver.factorial(5)).to eq(120)
+  end
+  it 'Factorial of zero' do
+    expect(solver.factorial(0)).to eq(1)
+  end
 
-    it 'Factorial of a negative number' do
-        expect{solver.factorial(-7)}.to raise_error(ArgumentError)
-    end
+  it 'Factorial of a negative number' do
+    expect { solver.factorial(-7) }.to raise_error(ArgumentError)
+  end
 end
 
 describe 'reverse' do
-    it 'Reverse of a string' do
-        
-        expect(solver.reverse('hello')).to eq('olleh')
-    end
+  it 'Reverse of a string' do
+    expect(solver.reverse('hello')).to eq('olleh')
+  end
 end
