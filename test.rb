5c1d6ff4f07c8f19ddaed1262b4ffb66aed4dd43
AdnanOlarmmi <olarmmi@gmail.com>
2023-01-06 10:16:24 +0100

    Add Factorial Tdd test

--- test.rb
+++ test.rb
@@ -1,2 +1,15 @@
 require './solver.rb'
 solver = Solver.new
+
+describe 'factorial' do
+    it 'Factorial of a number' do
+        expect(solver.factorial(5)).to eq(120)
+    end
+    it 'Factorial of zero' do
+        expect(solver.factorial(0)).to eq(1)
+    end
+
+    it 'Factorial of a negative number' do
+        expect{solver.factorial(-7)}.to raise_error(ArgumentError)
+    end
+end
