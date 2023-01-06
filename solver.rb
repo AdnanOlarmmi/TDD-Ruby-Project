a53e073699a4913b1c22933ce017d85e006d5718
Aminu Rabiu <arabiu033@gmail.com>
2023-01-06 10:36:29 +0100

    Provide the main code tested in the TDD mode

---
+++ solver.rb
@@ -0,0 +1,27 @@
+class Solver
+  def factorial(num)
+    if num.negative?
+      raise ArgumentError, 'Negative number'
+    elsif num.zero?
+      1
+    else
+      num * factorial(num - 1)
+    end
+  end
+
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'FizzBuzz'
+    elsif (num % 3).zero?
+      'Fizz'
+    elsif (num % 5).zero?
+      'Buzz'
+    else
+      num
+    end
+  end
+end
